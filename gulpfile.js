050930a37c58faeab80d2e7c25ffeb82067e5d2e
Rafael Bicalho <rbmbika@gmail.com>
2017-10-18 19:17:16 -0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+// Gulpfile
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var data = require('gulp-data');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+
+// Serve - Starts the server and watch files changes
+gulp.task('serve', ['sass', 'html', 'images', 'js'], function() {
+  browserSync.init({
+    server: {baseDir: './dist'},
+    open: false
+  });
+
+  gulp.watch('src/scss/**/*.scss', ['sass-watch']);
+  gulp.watch('src/js/*.js', ['js-watch']);
+  gulp.watch('src/templates/*.html', ['html-watch']);
+});
+
+// HTML
+gulp.task('html', function() {
+  return gulp
+    .src('src/templates/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html-watch', ['html'], function(done) {
+  browserSync.reload();
+  done();
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp
+    .src('src/images/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+// JS
+gulp.task('js', function() {
+  return gulp
+    .src('src/js/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-watch', ['js'], function(done) {
+  browserSync.reload();
+  done();
+});
+
+// Sass - Compiles sass files to css files
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass()).on('error', sass.logError)
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass-watch', ['sass'], function(done) {
+  browserSync.reload();
+  done();
+});
+
+// Clean - Deletes the dist folder
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+// Default task
+gulp.task('default', ['serve']);
